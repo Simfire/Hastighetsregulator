5be97131f203f140d2b2871599a106dd81aabc18
Anders Hansson <simfire@hotmail.se>
2021-02-07 19:45:56 +0100

    Issue with lcd is fixed. Problem with wheelSensor test fixed (even if the sensor was ok it entered the else loop.)

--- H-regulator.ino
+++ H-regulator.ino
@@ -10,11 +10,15 @@
 //************************************************************************************************************
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);// LCD pin connections
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+// rs = register select, en = enable, d0 - d3 = 4 bit data
+const int  rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2 ;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 bool wheelSensorOK;        // Status of wheel sensor.
 const int wheelSensorTestPin1 = 8;  // For testing the condition of the wheelsensor.
-const int wheelSensorTestPin2 = 9;  //              - " -
+const int wheelSensorTestPin2 = 11;  //              - " -
 const int pulsePin = 12;            // Pin for reading the puls from the wheelsensor.
 const int ignitionPin = 10;         // Controls the status of the ignitioncoil.
 int pulseHigh;                      // High duration time of pulse.
@@ -29,51 +33,60 @@ void setup() {
   pinMode(wheelSensorTestPin2,INPUT);
   pinMode(pulsePin,INPUT);
   pinMode(ignitionPin,OUTPUT);
-  wheelSensorOK == false;
-  lcd.begin(16, 2);
+  wheelSensorOK == true;
+  lcd.begin(16,2);
   lcd.clear();
  
   // Continuity test for the wheel sensor. If it's OK the ignition coil is enabled
   // otherwise the coil is dissabled.
-  digitalWrite(ignitionPin,LOW);            // Power to ignition coil OFF.
+  digitalWrite(ignitionPin,HIGH);            // Power to ignition coil OFF.
   digitalWrite(wheelSensorTestPin1,HIGH);   // Initiates test
-  delay(500);
-  digitalRead(wheelSensorTestPin2);
-  if (wheelSensorTestPin2 == HIGH){
-    wheelSensorOK == true;
-    lcd.setCursor(0,0);
+  lcd.setCursor(0,0);
+  lcd.print("Hjulsensortest");
+  delay(1000);
+ 
+  if (digitalRead(wheelSensorTestPin2) == HIGH){
+    wheelSensorOK = true;
+    lcd.clear();
+    lcd.setCursor(1,0);
     lcd.print("Hjulsensor OK!");
     delay(3000);
     lcd.setCursor(0,1);
-    lcd.print(“Kör försiktigt!”);
-delay(3000);
+    lcd.print("Kor forsiktigt!");
+    delay(3000);
     }
    else {
-wheelSensorOK == false;
-digitalWrite(ignitionPin, LOW);          
-    while(){
-lcd.setCursor(0,0);
+    wheelSensorOK = false;
+    digitalWrite(ignitionPin, LOW);          
+   while(1){
+    lcd.clear();
+    lcd.setCursor(1,0);
     lcd.print("Hjulsensor ur");
-    lcd.setCursor(0,1);
+    lcd.setCursor(3,1);
     lcd.print("funktion!");
-    delay(1000);
-    lcd.setCursor(0,0);
-    lcd.print("  Startspärr  ");
-    lcd.setCursor(0,1);
-    lcd.print("  aktiverad!  ");
-    delay(5000);
+    delay(2000);
+    lcd.clear();
+    lcd.setCursor(3,0);
+    lcd.print("Startsparr");
+    lcd.setCursor(3,1);
+    lcd.print("aktiverad!");
+    delay(2000);
+//    }
     }
-   }
-  lcd.clear();
-  digitalWrite(wheelsensorTestPin1,LOW);    // Abort test
+//  lcd.clear();
+  digitalWrite(wheelSensorTestPin1,LOW);    // Abort test
+  lcd.setCursor(0,1);
+    lcd.print("Hjulsensor OK!");
+    delay(3000);
+  }
 }
-
 void loop() {
-
+//  lcd.clear();
+  
   pulseHigh = pulseIn(pulsePin, HIGH);
   pulseLow = pulseIn(pulsePin, LOW);
   pulseTotal = pulseHigh + pulseLow;        // Time period of the pulse in micro seconds.
- 
+  
   if (pulseTotal == 0) {
     digitalWrite(ignitionPin, HIGH);
     frequency = 0;
@@ -81,15 +94,25 @@ void loop() {
   else {
     frequency = 1000000/ pulseTotal;      // Frequency in Hertz. (Hz)
     
-    if (frequency   < 91) {                       // Equals a velocity of 13 km/h
+    if (frequency < 130) {                       // Equals a velocity of 13 km/h
     digitalWrite(ignitionPin, HIGH);
+//    lcd.setCursor(0,0);
+//    lcd.print(frequency);
+//    lcd.print("  Hz");
+//    delay(500);
     }
-    if (frequency   > 112) {            // Equals a velocity of 16 km/h
+    if (frequency > 140) {            // Equals a velocity of 16 km/h
     digitalWrite(ignitionPin, LOW);
     }
-velocity = frequency * 0,1425;            // Speed in km/h.
-    lcd.setCursor(0,0);
-    lcd.print(velocity);
-    lcd.print(" km/h");
+  velocity = frequency * 0.1425; // Speed in km/h.
+  lcd.clear();
+  lcd.setCursor(0,0);
+    lcd.print(frequency);
+    lcd.print("  Hz");
+  lcd.setCursor(0,1);
+  lcd.print(velocity);
+  lcd.print("  km/h");
+  delay(100);
+    
           }
 }
